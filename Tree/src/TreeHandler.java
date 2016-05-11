785d71f5dc0e9daa2ea5d051ff95d968697a75bb
Sanjeev Kumar <skumar6@umbc.edu>
2016-05-11 14:34:45 -0400

    iterative preOrder added

--- Tree/src/TreeHandler.java
+++ Tree/src/TreeHandler.java
@@ -8,18 +8,20 @@ public class TreeHandler {
        t.insert(4);
        t.insert(15);
        t.insert(3);
-       t.insert(12);
+       t.insert(16);
 
-       t.inOrder(t.getRoot());
-       System.out.println("");
-//
-//       t.preOrder(t.getRoot());
+//       t.inOrder(t.getRoot());
 //       System.out.println("");
+//
+       t.preOrder(t.getRoot());
+       System.out.println("");
 //
 //       t.postOrder(t.getRoot());
 //       System.out.println("");
 
-       t.iterativeInOrder(t.getRoot());
-       //System.out.println("");
+//       t.iterativeInOrder(t.getRoot());
+//       System.out.println("");
+//       t.iterativePostOeder(t.getRoot());
+       t.iterativePreOrder(t.getRoot());
    }
 }
